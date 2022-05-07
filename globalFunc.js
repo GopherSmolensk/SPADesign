60ef5873433d187d9649afa6a1712bb40274e72e
Dmitriy <dm.fred.nik@gmail.com>
2022-05-07 17:40:18 +0300

    add.

---
+++ globalFunc.js
@@ -0,0 +1,19 @@
+
+
+// function vehicle(car, motorbike, sun ) {
+
+//   if (sun) {
+//     motorbike();
+//   } else {
+//     car();
+//   }
+
+// }
+
+
+
+function car(fff) {
+  console.log("Хорошая машина");
+
+  fff("Но дорогая");
+}
---
+++ main.js
@@ -0,0 +1,77 @@
+// console.log('хай! май!');
+
+// function sayHello() {
+//   console.log('hi');
+// }
+// // sayHello();
+
+
+// // var clickMeButton = document.querySelector("#click-me");
+// one.addEventListener("click", sayHello);
+
+
+
+// function handler1() {
+//   console.log('Спасибо!'); 
+// };
+
+// function handler2() {
+//   console.log('Спасибо ещё раз!');
+// }
+
+// two.onclick = () => console.log("Привет");
+// two.addEventListener("click", handler1); // Спасибо!
+// two.addEventListener("click", handler2); // Спасибо ещё раз!
+// two.addEventListener("click", sayHello); // Спасибо ещё раз!
+
+// setTimeout(handler2, 5000);
+
+// function nameUser() {
+//   console.log("Дмитрий Фёдоров");
+// }
+
+// function nameAddress() {
+//   console.log("ул.Кловская д.27а кв 5");
+// }
+
+// function City() {
+//   console.log("Смоленск");
+// }
+
+// function Country() {
+//   console.log("Russian Federation");
+// }
+
+// function userData (x, y) {
+//   console.log("Привет что ты знаешь обо мне");
+//   x();
+//   y();
+// }
+
+// userData(nameAddress, Country );
+
+// function plus(a, b) {
+//   console.log(a + b);
+// }
+
+// plus(5315541551, 5151656552651458);
+
+// function car() {
+//   console.log("Mersedes");
+// }
+// function motorbike() {
+//   console.log("Yamaha");
+// }
+
+// vehicle(car, motorbike, false);
+
+
+
+function mersedes(fff) {
+  console.log(fff);
+}
+
+
+
+car(mersedes);
+
